e16c5e5592869495d108000de0379cac21cd0216
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# How to: Bind a ButtonEdit buttons to a collection in a view model
+
+
+<p>This example shows how to create <strong>ButtonEdit</strong> buttons based on a collection in a view model and define the button appearance using the <strong>DataTemplate</strong> and <strong>DataTemplateSelector</strong> classes.</p>
+
+
+<h3>Description</h3>
+
+This is done by binding the <a href="https://documentation.devexpress.com/WPF/DevExpressXpfEditorsButtonEdit_ButtonsSourcetopic.aspx">ButtonsSource</a> property and specifying a custom <a href="https://documentation.devexpress.com/WPF/DevExpressXpfEditorsButtonEdit_ButtonTemplatetopic.aspx">ButtonTemplate</a>.
+
+<br/>
+
+
---
+++ VB/DXSample/ViewModels/ButtonViewModel.vb
@@ -0,0 +1,33 @@
+﻿Imports DevExpress.Mvvm
+Imports System.Windows.Input
+
+Namespace DXSample.ViewModels
+	Public Class ButtonViewModel
+		Inherits ViewModelBase
+
+		Public Property Content() As String
+			Get
+				Return GetProperty(Function() Content)
+			End Get
+			Set(ByVal value As String)
+				SetProperty(Function() Content, value)
+			End Set
+		End Property
+		Public Property Command() As ICommand
+			Get
+				Return GetProperty(Function() Command)
+			End Get
+			Set(ByVal value As ICommand)
+				SetProperty(Function() Command, value)
+			End Set
+		End Property
+		Public Property IsLeft() As Boolean
+			Get
+				Return GetProperty(Function() IsLeft)
+			End Get
+			Set(ByVal value As Boolean)
+				SetProperty(Function() IsLeft, value)
+			End Set
+		End Property
+	End Class
+End Namespace
